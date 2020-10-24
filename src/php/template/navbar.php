4f94459e5382c10e8bb04faaaa3951470602ec8b
stefanusgusega <gunawanstefanus006@gmail.com>
2020-10-24 14:12:55 +0700

    merge and implement homepage

--- src/php/template/navbar.php
+++ src/php/template/navbar.php
@@ -4,6 +4,7 @@
             <a href="./homepage">Home</a>
             <a id ="add"   href="#"></a>
             <a id="history" href="./history"></a>
+            <input type="text" id="search"placeholder="Search..">
             <a id="right" href="./logout">Logout</a>
         </div>
 
