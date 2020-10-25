9a469582ff648ac2c20decf3ea34de220c12d43f
Felicia Gojali <13518101@std.stei.itb.ac.id>
2020-10-25 14:42:28 +0700

    search

--- src/php/search_result.php
+++ src/php/search_result.php
@@ -47,22 +47,25 @@
             $amountRemaining = $row['amountRemaining'];
             $description = $row['description'];
             $image_path = $row['path'];
+            $details = "/details/" . $id;
             echo
             "<div class=\"result-container\" id=\"result-container-$id\">
-            <span class=\"item-image-container\">
+            <a href=$details class=\"item-image-container name\">
               <img class=\"item-image\" src=\"../../assets/images/$image_path\">
-            </span>
+            </a>
             <span class=\"item-detail\">
+            <a class=\"name\" href=$details>
               <div class=\"item-name\">$name</div>
+              </a> 
               <div class=\"item-amount-sold\">Amount sold: $amountSold</div>
               <div class=\"item-price\">Price: Rp $price</div>
               <div class=\"item-amount-remaining\">Amount remaining: $amountRemaining</div>
               <div class=\"item-description\">
-                <a>Description</a>
+                <p>Description</p>
                 <div class=\"item-description\">$description</div>
               </div>
+              </a>
             </span>
-            <input type=\"submit\" value=\"BUY\">
             <br>
             
             
